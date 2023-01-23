2c5fa5441f41a927a37313885cb5617d94f73aa2
YunitaHerlinia <101621560+YunitaHerlinia@users.noreply.github.com>
2023-01-23 20:05:47 +0700

    Add files via upload

---
+++ henny.03.js
@@ -0,0 +1,8 @@
+void main() {
+	var age = 20;
+if (age >= 18) {
+	print("anda sudah cukup umur");
+  } else {
+  	print("anda belum cukup umur");
+  }
+}
---
+++ henny.04.js
@@ -0,0 +1,8 @@
+void main() {
+	var numbers = [5, 2, 10, 8, 7]
+	var max = numbers[0];
+	// TODO
+
+	print("Nilain terbesar dari array adalah $max");
+
+}
